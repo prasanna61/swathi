67b7f948145daf9ad3bfafc84e0651d64d8f1b4f
LAKSHMI PRASANNA TANGUTURI <88602508+prasanna61@users.noreply.github.com>
2023-04-19 12:55:21 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,8 @@
+b=int(input('enter the base of triangle'))
+h=int(input('Enter the height of triangle'))
+print('area of triangle is ',0.5*b*h)
+s=int(input('Enter the side of a square'))
+print('area of square is ',s*s)
+l=int(input('enter the length of rect'))
+w=int(input('enter the width of rect'))
+print('area of rect is ',l*w)
